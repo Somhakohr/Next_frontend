575a0f991fec2d16f723516e703f9ebe6d134860
anmol-somhako <anmol.verma@somhako.com>
2022-11-23 10:06:59 +0530

    dashbaord candidate desinged

---
+++ components/job-card.js
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import googleIcon from "../public/images/google-icon.png";
+
+export default function JobCard() {
+    return (
+        <>
+            <div className="bg-[#f4f4f4] p-5 border border-2 border-slate-300 rounded-[25px]">
+                <div className="flex mb-8">
+                    <div className="bg-white rounded-full p-2.5 flex items-center justify-center w-[50px] h-[50px]">
+                        <Image src={googleIcon} alt="Google" className="w-full" />
+                    </div>
+                    <div className="pl-3 w-[calc(100%-60px)]">
+                        <h3 className="font-bold text-md mb-1">Product Designer</h3>
+                        <h5 className="font-medium text-sm">Google</h5>
+                    </div>
+                </div>
+                <div className="text-[#787878] text-sm flex flex-wrap border-b border-slate-300">
+                    <p className="w-full sm:max-w-[50%] mb-3">
+                        Place: Bangalore
+                    </p>
+                    <p className="w-full sm:max-w-[50%] mb-3 sm:text-right">
+                        Mid Career
+                    </p>
+                    <p className="w-full sm:max-w-[50%] mb-3">
+                        Fulltime , Hybrid
+                    </p>
+                    <p className="w-full sm:max-w-[50%] mb-3 sm:text-right">
+                        Rs 550000/Yearly
+                    </p>
+                </div>
+                <div className="flex items-center justify-between pt-4 text-sm">
+                    <p>
+                        5 days ago
+                    </p>
+                    <Link href="#" className="text-[#6D27F9] hover:underline hover:text-black">View Job</Link>
+                </div>
+            </div>
+        </>
+    )
+}
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -88,131 +88,131 @@ export default function SignIn() {
       </Head>
       <main className="bg-[#FAF8FF]">
       {/* ALERT EXAMPLE */}
-      {/* <div class="flex flex-col justify-center">
-  <div class="bg-blue-600 shadow-lg mx-auto w-96 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block mb-3" id="static-example" role="alert" aria-live="assertive" aria-atomic="true" data-mdb-autohide="false">
-    <div class="bg-blue-600 flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-blue-500 rounded-t-lg">
-      <p class="font-bold text-white flex items-center">
-        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="info-circle" class="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      {/* <div className="flex flex-col justify-center">
+  <div className="bg-blue-600 shadow-lg mx-auto w-96 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block mb-3" id="static-example" role="alert" aria-live="assertive" aria-atomic="true" data-mdb-autohide="false">
+    <div className="bg-blue-600 flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-blue-500 rounded-t-lg">
+      <p className="font-bold text-white flex items-center">
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="info-circle" className="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
           <path fill="currentColor" d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"></path>
         </svg>
         MDBootstrap</p>
-      <div class="flex items-center">
-        <p class="text-white opacity-90 text-xs">11 mins ago</p>
-        <button type="button" class="btn-close btn-close-white box-content w-4 h-4 ml-2 text-white border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-white hover:opacity-75 hover:no-underline" data-mdb-dismiss="toast" aria-label="Close"></button>
+      <div className="flex items-center">
+        <p className="text-white opacity-90 text-xs">11 mins ago</p>
+        <button type="button" className="btn-close btn-close-white box-content w-4 h-4 ml-2 text-white border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-white hover:opacity-75 hover:no-underline" data-mdb-dismiss="toast" aria-label="Close"></button>
       </div>
     </div>
-    <div class="p-3 bg-blue-600 rounded-b-lg break-words text-white">
+    <div className="p-3 bg-blue-600 rounded-b-lg break-words text-white">
       Hello, world! This is a toast message.
     </div>
   </div>
-  <div class="bg-green-500 shadow-lg mx-auto w-96 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block mb-3" id="static-example" role="alert" aria-live="assertive" aria-atomic="true" data-mdb-autohide="false">
-    <div class="bg-green-500 flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-green-400 rounded-t-lg">
-      <p class="font-bold text-white flex items-center">
-        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check-circle" class="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+  <div className="bg-green-500 shadow-lg mx-auto w-96 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block mb-3" id="static-example" role="alert" aria-live="assertive" aria-atomic="true" data-mdb-autohide="false">
+    <div className="bg-green-500 flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-green-400 rounded-t-lg">
+      <p className="font-bold text-white flex items-center">
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check-circle" className="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
           <path fill="currentColor" d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path>
         </svg>
         MDBootstrap</p>
-      <div class="flex items-center">
-        <p class="text-white opacity-90 text-xs">11 mins ago</p>
-        <button type="button" class="btn-close btn-close-white box-content w-4 h-4 ml-2 text-white border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-white hover:opacity-75 hover:no-underline" data-mdb-dismiss="toast" aria-label="Close"></button>
+      <div className="flex items-center">
+        <p className="text-white opacity-90 text-xs">11 mins ago</p>
+        <button type="button" className="btn-close btn-close-white box-content w-4 h-4 ml-2 text-white border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-white hover:opacity-75 hover:no-underline" data-mdb-dismiss="toast" aria-label="Close"></button>
       </div>
     </div>
-    <div class="p-3 bg-green-500 rounded-b-lg break-words text-white">
+    <div className="p-3 bg-green-500 rounded-b-lg break-words text-white">
       Hello, world! This is a toast message.
     </div>
   </div>
-  <div class="bg-yellow-500 shadow-lg mx-auto w-96 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block mb-3" id="static-example" role="alert" aria-live="assertive" aria-atomic="true" data-mdb-autohide="false">
-    <div class="bg-yellow-500 flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-yellow-400 rounded-t-lg">
-      <p class="font-bold text-white flex items-center">
-        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="exclamation-triangle" class="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+  <div className="bg-yellow-500 shadow-lg mx-auto w-96 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block mb-3" id="static-example" role="alert" aria-live="assertive" aria-atomic="true" data-mdb-autohide="false">
+    <div className="bg-yellow-500 flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-yellow-400 rounded-t-lg">
+      <p className="font-bold text-white flex items-center">
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="exclamation-triangle" className="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
           <path fill="currentColor" d="M569.517 440.013C587.975 472.007 564.806 512 527.94 512H48.054c-36.937 0-59.999-40.055-41.577-71.987L246.423 23.985c18.467-32.009 64.72-31.951 83.154 0l239.94 416.028zM288 354c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z"></path>
         </svg>
         MDBootstrap</p>
-      <div class="flex items-center">
-        <p class="text-white opacity-90 text-xs">11 mins ago</p>
-        <button type="button" class="btn-close btn-close-white box-content w-4 h-4 ml-2 text-white border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-white hover:opacity-75 hover:no-underline" data-mdb-dismiss="toast" aria-label="Close"></button>
+      <div className="flex items-center">
+        <p className="text-white opacity-90 text-xs">11 mins ago</p>
+        <button type="button" className="btn-close btn-close-white box-content w-4 h-4 ml-2 text-white border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-white hover:opacity-75 hover:no-underline" data-mdb-dismiss="toast" aria-label="Close"></button>
       </div>
     </div>
-    <div class="p-3 bg-yellow-500 rounded-b-lg break-words text-white">
+    <div className="p-3 bg-yellow-500 rounded-b-lg break-words text-white">
       Hello, world! This is a toast message.
     </div>
   </div>
-  <div class="bg-red-600 shadow-lg mx-auto w-96 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block mb-3" id="static-example" role="alert" aria-live="assertive" aria-atomic="true" data-mdb-autohide="false">
-    <div class="bg-red-600 flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-red-500 rounded-t-lg">
-      <p class="font-bold text-white flex items-center">
-        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times-circle" class="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+  <div className="bg-red-600 shadow-lg mx-auto w-96 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block mb-3" id="static-example" role="alert" aria-live="assertive" aria-atomic="true" data-mdb-autohide="false">
+    <div className="bg-red-600 flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-red-500 rounded-t-lg">
+      <p className="font-bold text-white flex items-center">
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times-circle" className="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
           <path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z"></path>
         </svg>
         MDBootstrap</p>
-      <div class="flex items-center">
-        <p class="text-white opacity-90 text-xs">11 mins ago</p>
-        <button type="button" class="btn-close btn-close-white box-content w-4 h-4 ml-2 text-white border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-white hover:opacity-75 hover:no-underline" data-mdb-dismiss="toast" aria-label="Close"></button>
+      <div className="flex items-center">
+        <p className="text-white opacity-90 text-xs">11 mins ago</p>
+        <button type="button" className="btn-close btn-close-white box-content w-4 h-4 ml-2 text-white border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-white hover:opacity-75 hover:no-underline" data-mdb-dismiss="toast" aria-label="Close"></button>
       </div>
     </div>
-    <div class="p-3 bg-red-600 rounded-b-lg break-words text-white">
+    <div className="p-3 bg-red-600 rounded-b-lg break-words text-white">
       Hello, world! This is a toast message.
     </div>
   </div>
       </div> */} 
-      {/* <div class="flex items-center justify-between max-w-xs p-4 bg-white border rounded-md shadow-sm">
-        <div class="flex items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-green-500" viewBox="0 0 20 20" fill="currentColor">
+      {/* <div className="flex items-center justify-between max-w-xs p-4 bg-white border rounded-md shadow-sm">
+        <div className="flex items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-green-500" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd"
               d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
               clip-rule="evenodd" />
           </svg>
-          <p class="ml-3 text-sm font-bold text-green-600">Successfully Toast Message !</p>
+          <p className="ml-3 text-sm font-bold text-green-600">Successfully Toast Message !</p>
         </div>
-        <span class="inline-flex items-center cursor-pointer">
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-gray-600" fill="none" viewBox="0 0 24 24"
+        <span className="inline-flex items-center cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-gray-600" fill="none" viewBox="0 0 24 24"
             stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
         </span>
       </div>
-      <div class="flex items-center justify-between max-w-xs p-4 bg-white border rounded-md shadow-sm">
-        <div class="flex items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-blue-600" viewBox="0 0 20 20" fill="currentColor">
+      <div className="flex items-center justify-between max-w-xs p-4 bg-white border rounded-md shadow-sm">
+        <div className="flex items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-blue-600" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd"
               d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
               clip-rule="evenodd" />
           </svg>
-          <p class="ml-3 text-sm font-bold text-blue-600">Info Toast Message !</p>
+          <p className="ml-3 text-sm font-bold text-blue-600">Info Toast Message !</p>
         </div>
-        <span class="inline-flex items-center cursor-pointer">
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-gray-600" fill="none" viewBox="0 0 24 24"
+        <span className="inline-flex items-center cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-gray-600" fill="none" viewBox="0 0 24 24"
             stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
         </span>
       </div>
-      <div class="flex items-center justify-between max-w-xs p-4 bg-white border rounded-md shadow-sm">
-        <div class="flex items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-yellow-600" viewBox="0 0 20 20" fill="currentColor">
+      <div className="flex items-center justify-between max-w-xs p-4 bg-white border rounded-md shadow-sm">
+        <div className="flex items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-yellow-600" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd"
               d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
               clip-rule="evenodd" />
           </svg>
-          <p class="ml-3 text-sm font-bold text-yellow-600">Warning Toast Message !</p>
+          <p className="ml-3 text-sm font-bold text-yellow-600">Warning Toast Message !</p>
         </div>
-        <span class="inline-flex items-center cursor-pointer">
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-gray-600" fill="none" viewBox="0 0 24 24"
+        <span className="inline-flex items-center cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-gray-600" fill="none" viewBox="0 0 24 24"
             stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
         </span>
       </div>
-      <div class="flex items-center justify-between max-w-xs p-4 bg-white border rounded-md shadow-sm">
-        <div class="flex items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-red-600" viewBox="0 0 20 20" fill="currentColor">
+      <div className="flex items-center justify-between max-w-xs p-4 bg-white border rounded-md shadow-sm">
+        <div className="flex items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-red-600" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd"
               d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
               clip-rule="evenodd" />
           </svg>
-          <p class="ml-3 text-sm font-bold text-red-600">Error Toast Message !</p>
+          <p className="ml-3 text-sm font-bold text-red-600">Error Toast Message !</p>
         </div>
-        <span class="inline-flex items-center cursor-pointer">
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-gray-600" fill="none" viewBox="0 0 24 24"
+        <span className="inline-flex items-center cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-gray-600" fill="none" viewBox="0 0 24 24"
             stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
