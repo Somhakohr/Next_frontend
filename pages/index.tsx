0b6bf6ce2a801c57e00ea2c8a2bf0a5698bdaa09
NAMAN <200120107502@git.org.in>
2022-11-28 22:23:25 +0530

    commit message

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,33 +8,6 @@ import useStore from "../hooks/useStore";
 
 function Home(props) {
   const { session } = props;
-  // console.log(session);
-  const {router,setUserObj,setUType,axiosInstance,utype} = useStore();
-  // useEffect(function () {
-  //   console.log("abc", session);
-  //   if (session) {
-  //     axiosInstance.post('/auth/getuser/', {
-  //       email: session.user.email,
-  //     }).then(async (response) => {
-  //       console.log(response);
-  //       if (response.data.user != "no data") {
-  //         setUType(response.data.type);
-  //         setUserObj(response.data.user);
-  //         router.push("/" + utype);
-  //       }
-  //       else {
-  //         setUType('');
-  //         setUserObj({});
-  //       }
-  //     }).catch((err) => {
-  //       console.log(err);
-  //       setUType('');
-  //       setUserObj({});
-  //     });
-  //   }
-
-  // }, [session]);
-  
   return (
     <>
     <section className="py-4">
@@ -43,19 +16,6 @@ function Home(props) {
         </div>
       </section>
       {session?<>{session.user.email}</>:<></>}
-    {/* {
-      session?
-      <>
-       Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-        {session.accessToken && <pre>User has access token</pre>}
-      </>:
-      <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-      <pre>{"User is not logged in"}</pre>
-      </>
-    } */}
     </>
   )
 }
