7c50961f219a8320c14fb0b355c0bf2609640152
anmol-somhako <anmol.verma@somhako.com>
2022-12-06 16:51:18 +0530

    share preview job popup added

--- components/org-jobs-card.tsx
+++ components/org-jobs-card.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Fragment, useRef, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
 import googleIcon from "../public/images/google-icon.png";
 
 export default function OrganisationJobsCard() {
+    const [shareJob, shareJobPopupOpen] = useState(false)
+    const cancelButtonRef = useRef(null)
     return (
         <>
             <div className="bg-[#f4f4f4] border border-2 border-slate-300 rounded-[25px] overflow-hidden">
@@ -17,7 +21,7 @@ export default function OrganisationJobsCard() {
                                 <h5 className="font-medium text-sm my-1 mr-6">Google</h5>
                                 <div className="flex items-center text-[12px] mt-[2px]">
                                     <p className="text-[#7e7e7e] mr-2">JB-491170</p>
-                                    <button type="button">
+                                    <button type="button" onClick={() => shareJobPopupOpen(true)}>
                                         <i className="fa-solid fa-share text-[#6D27F9]"></i>
                                     </button>
                                 </div>
@@ -64,6 +68,75 @@ export default function OrganisationJobsCard() {
                     </div>
                 </div>
             </div>
+            <Transition.Root show={shareJob} as={Fragment}>
+                <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={shareJobPopupOpen}>
+                    <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                    >
+                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                    </Transition.Child>
+
+                    <div className="fixed inset-0 z-10 overflow-y-auto">
+                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                        <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        enterTo="opacity-100 translate-y-0 sm:scale-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                        leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        >
+                        <Dialog.Panel className="relative transform overflow-hidden rounded-[30px] bg-[#FBF9FF] text-left shadow-xl transition-all sm:my-8 w-full sm:max-w-md">
+                            <div className="p-8">
+                                <div className="flex items-center justify-between mb-8">
+                                    <h4 className="leading-none font-semibold text-xl">Share Job Via</h4>
+                                    <button type="button" className="leading-none" onClick={() => shareJobPopupOpen(false)}>
+                                        <i className="fa-solid fa-xmark"></i>
+                                    </button>
+                                </div>
+                                <div className="shadow-md rounded-[20px] p-6">
+                                    <ul className="flex items-center flex-wrap justify-center text-center text-[#6D27F9] text-xl">
+                                        <li className="w-[33.33%] px-[10px] mb-2">
+                                            <button type="button" className="hover:text-black">
+                                                <i className="fa-brands fa-linkedin-in"></i>
+                                            </button>
+                                        </li>
+                                        <li className="w-[33.33%] px-[10px] mb-2">
+                                            <button type="button" className="hover:text-black">
+                                                <i className="fa-brands fa-twitter"></i>
+                                            </button>
+                                        </li>
+                                        <li className="w-[33.33%] px-[10px] mb-2">
+                                            <button type="button" className="hover:text-black">
+                                                <i className="fa-brands fa-facebook-f"></i>
+                                            </button>
+                                        </li>
+                                        <li className="w-[33.33%] px-[10px] mb-2">
+                                            <button type="button" className="hover:text-black">
+                                                <i className="fa-brands fa-telegram"></i>
+                                            </button>
+                                        </li>
+                                        <li className="w-[33.33%] px-[10px] mb-2">
+                                            <button type="button" className="hover:text-black">
+                                                <i className="fa-regular fa-copy"></i>
+                                            </button>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
+                    </div>
+                </Dialog>
+            </Transition.Root>
         </>
     )
 }
