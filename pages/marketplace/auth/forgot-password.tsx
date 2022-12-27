74ad1cfebbde67074cd523f1a1433ebb9edd14cb
NAMAN <200120107502@git.org.in>
2022-12-27 15:11:33 +0530

    27-12-22

--- pages/marketplace/auth/forgot-password.tsx
+++ pages/marketplace/auth/forgot-password.tsx
@@ -1,8 +1,38 @@
+import axios from "axios";
 import Head from "next/head";
 import Link from 'next/link'
+import { useState } from "react";
 import Auth_Slider from "../../../components/auth-slider";
+import toastcomp from "../../../components/toast";
 
 export default function ForgotPassword() {
+
+  const [email,setemail] = useState('')
+
+  function validbtn(){
+    return email.length > 0
+  }
+
+  const axiosInstance = axios.create({
+    baseURL: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_PROD_BACKEND_BASE : process.env.NEXT_PUBLIC_DEV_BACKEND_BASE,
+    timeout: 5000,
+    headers: {
+        // 'Authorization': "JWT " + access_token,
+        'Content-Type': 'application/json',
+        'accept': 'application/json'
+    }
+  });
+
+  async function forgetpass() {
+    await axiosInstance.post('/auth/send-reset-password-email/',{'email':email}).then((response)=>{
+      toastcomp("Forget Password Mail Sent","success")
+    }).catch((err)=>{
+      // console.log(err);
+      toastcomp("Forget Password Error","error")      
+    });
+  }
+
+
   return (
     <>
       <Head>
@@ -21,17 +51,17 @@ export default function ForgotPassword() {
                       <h1 className="font-medium text-xl md:text-3xl mb-12">
                         Forgot Password
                       </h1>
-                      <form className="mb-16">
+                      <div className="mb-16">
                         <div className="mb-6">
                           <label htmlFor="email" className="font-medium mb-2 leading-none inline-block">Email</label>
-                          <input id="email" type="email" className="w-full rounded-full border-slate-300" />
+                          <input id="email" type="email" className="w-full rounded-full border-slate-300" value={email} onChange={(e)=>setemail(e.target.value)} />
                         </div>
                         <div className="flex flex-wrap items-center justify-between md:flex-row flex-col">
-                          <button type="submit" className="bg-gradient-to-r from-[#6D27F9] to-[#9F09FB] text-white font-bold rounded-full py-2.5 px-6 md:min-w-[200px] transition-all hover:from-[#391188] hover:to-[#391188]">
+                          <button type="submit" className="bg-gradient-to-r from-[#6D27F9] to-[#9F09FB] text-white font-bold rounded-full py-2.5 px-6 md:min-w-[200px] transition-all hover:from-[#391188] hover:to-[#391188]" disabled={!validbtn()} onClick={(e)=>forgetpass()}>
                             Submit
                           </button>
                         </div>
-                      </form>
+                      </div>
                       <div className="relative mb-8">
                         <hr className="border-slate-600" />
                         <span className="text-center absolute top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%] bg-white px-2 md:px-5">Not a member yet?</span>
